d4682050c6c1e15d00dd255e5fbeafd69c1a7592
Noah Doersing <doersino@gmail.com>
2016-10-06 15:15:25 +0200

    Use list functions instead of regex, increasing performance

--- src/Spell.hs
+++ src/Spell.hs
@@ -1,10 +1,3 @@
--- Haskell implementation of http://norvig.com/spell-correct.html
--- Note: Requires semi-recent version of the following packages:
---       base (duh!), containers, regex-posix, list-extras, data-ordlist
--- Note: This runs quite slow, so generate a file "small.txt" by running "cat
---       big.txt | head -n 1000 > small.txt" in your shell of choice.
--- Usage: ghci SpellCorrect.hs < correction "speling"
-
 module Spell where
 
 import Prelude hiding (words)
@@ -14,8 +7,8 @@ import Data.Maybe
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
+import Data.List.Split
 import Data.List.Extras.Argmax
-import Text.Regex.Posix
 import qualified Data.List.Ordered as Ordered
 
 import System.IO.Unsafe
@@ -27,8 +20,8 @@ import System.IO.Unsafe
 words :: Map.Map String Int
 words = Map.fromList [ (l, length l) | l <- Ordered.nub (sort words') ]
   where
-    text  = unsafePerformIO $ readFile "small.txt"
-    words'= getAllTextMatches $ map toLower text =~ "[a-zA-Z]+" :: [String]
+    text   = unsafePerformIO $ readFile "small.txt"
+    words' = filter (not . null) . splitWhen (not . isAlpha) $ map toLower text
 
 -- | Probability of @word@.
 p :: String -> Double
@@ -41,6 +34,7 @@ correction :: String -> String
 correction word = argmax p $ Set.toList $ candidates word
 
 -- | Generate possible spelling corrections for @word@.
+-- TODO fix
 candidates :: String -> Set.Set String
 candidates word = Set.unions [known [word], known $ Set.toList $ edits1 word, known $ Set.toList $ edits2 word, Set.singleton word]
 
@@ -48,7 +42,7 @@ candidates word = Set.unions [known [word], known $ Set.toList $ edits1 word, kn
 known :: [String] -> Set.Set String
 known words' = Set.fromList [ w | w <- words', Map.member w words]
 
--- All edits that are one edit away from @word@.
+-- | All edits that are one edit away from @word@.
 edits1 :: String -> Set.Set String
 edits1 word = Set.fromList $ deletes ++ transposes ++ replaces ++ inserts
   where
@@ -59,6 +53,6 @@ edits1 word = Set.fromList $ deletes ++ transposes ++ replaces ++ inserts
     replaces   = [ l ++ c : drop 1 r               | (l,r) <- splits, not (null r), c <- letters ]
     inserts    = [ l ++ c : r                      | (l,r) <- splits, c <- letters]
 
--- All edits that are two edits away from @word@.
+-- | All edits that are two edits away from @word@.
 edits2 :: String -> Set.Set String
 edits2 word = Set.fromList [ e2 | e1 <- Set.toList $ edits1 word, e2 <- Set.toList $ edits1 e1 ]
